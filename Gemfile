fc1db76df89dae1ad418da763b32516077bbb4bb
phiyen <phiyen15spt@gmail.com>
2019-09-05 13:43:06 +0700

    Chapter7: Sign up

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem "config"
 gem "jbuilder", "~> 2.5"
 gem "puma", "~> 3.11"
 gem "rails", "~> 5.2.3"
+gem "rails-controller-testing"
+gem "rails-i18n"
 gem "rubocop", "~> 0.54.0", require: false
 gem "sass-rails", "~> 5.0"
 gem "sqlite3"
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   validates :name,  presence: true,
     length: {maximum: Settings.maximum_lenght_name}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: maximum_lenght_email},
+  validates :email, presence: true,
+    length: {maximum: Settings.maximum_lenght_email},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true,
-    length: {minimum: minimum_lenght_password}
+    length: {minimum: Settings.minimum_lenght_password}
 end
