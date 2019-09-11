ad869e8f36171fa99a7903496b25fc0601a64a18
phiyen <phiyen15spt@gmail.com>
2019-09-11 11:43:43 +0700

    Chapter14: Following users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,20 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = t "controllers.users.following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render "show_follow"
+  end
+
+  def followers
+    @title = t "controllers.users.followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow"
+  end
+
   private
 
   def load_user
--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,14 @@ Rails.application.routes.draw do
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
-    get "password_resets/new"
-    get "password_resets/edit"
-    get "sessions/new"
-    resources :users
+    resources :users do
+      member do
+        get :following, :followers
+      end
+    end
     resources :account_activations, only: :edit
     resources :password_resets, except: [:destroy, :index, :show]
     resources :microposts, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
   end
 end
